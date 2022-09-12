1d38340d10f80185be72cb43049e888adf4f1d73
lyrahgames <lyrahgames@mailbox.org>
2022-09-12 04:07:00 +0200

    Start with 'named_tuple' template

---
+++ lyrahgames/xstd/named_tuple.hpp
@@ -0,0 +1,137 @@
+#pragma once
+#include <tuple>
+#include <utility>
+//
+#include <lyrahgames/xstd/forward.hpp>
+#include <lyrahgames/xstd/static_zstring.hpp>
+#include <lyrahgames/xstd/value_list.hpp>
+
+namespace lyrahgames::xstd {
+
+template <static_zstring... strings>
+using static_zstring_list = value_list<strings...>;
+
+namespace detail {
+template <typename type>
+struct is_static_zstring_list : std::false_type {};
+template <static_zstring... strings>
+struct is_static_zstring_list<static_zstring_list<strings...>>
+    : std::true_type {};
+}  // namespace detail
+
+template <typename list>
+constexpr bool is_static_zstring_list =
+    detail::is_static_zstring_list<list>::value;
+
+namespace instance {
+template <typename T>
+concept static_zstring_list = is_static_zstring_list<T>;
+}
+
+template <instance::static_zstring_list strings, typename... T>
+requires(strings::size == sizeof...(T))  //
+    struct named_tuple : std::tuple<T...> {
+  using names = strings;
+  using types = type_list<T...>;
+  using base = std::tuple<T...>;
+  using base::base;
+
+  static constexpr size_t size = sizeof...(T);
+  static constexpr bool empty = size == 0;
+
+  // operator base() const noexcept { return *this; }
+  // constexpr auto tuple() noexcept -> base { return *this; }
+
+  template <static_zstring name>
+  static constexpr auto contains = names::template contains<name>;
+};
+
+namespace detail {
+template <typename type>
+struct is_named_tuple : std::false_type {};
+template <instance::static_zstring_list names, typename... types>
+struct is_named_tuple<named_tuple<names, types...>> : std::true_type {};
+}  // namespace detail
+
+template <typename T>
+constexpr bool is_named_tuple = detail::is_named_tuple<T>::value;
+
+namespace instance {
+template <typename T>
+concept named_tuple = is_named_tuple<T>;
+}  // namespace instance
+
+namespace forwardable {
+template <typename T>
+concept named_tuple = instance::named_tuple<std::decay_t<T>>;
+}
+
+template <static_zstring... names, typename... types>
+constexpr auto auto_named_tuple(types&&... t) noexcept {
+  return named_tuple<static_zstring_list<names...>,
+                     std::unwrap_ref_decay_t<types>...>(
+      std::forward<types>(t)...);
+}
+
+template <static_zstring... names, typename... types>
+constexpr auto named_tuple_forward(types&&... t) noexcept {
+  return named_tuple<static_zstring_list<names...>, types&&...>(
+      std::forward<types>(t)...);
+}
+
+template <size_t index>
+constexpr decltype(auto) value(forwardable::named_tuple auto&& t) noexcept  //
+    requires(index < std::decay_t<decltype(t)>::names::size) {
+  return std::get<index>(forward<decltype(t)>(t));
+}
+
+template <static_zstring name>
+constexpr decltype(auto) value(forwardable::named_tuple auto&& t) noexcept  //
+    requires(std::decay_t<decltype(t)>::template contains<name>) {
+  using tuple_type = std::decay_t<decltype(t)>;
+  return std::get<tuple_type::names::template index<name>>(
+      std::forward<decltype(t)>(t));
+}
+
+namespace detail {
+template <static_zstring name, std::size_t... indices>
+constexpr auto push_back(forwardable::named_tuple auto&& t,
+                         auto&& x,
+                         std::index_sequence<indices...>) {
+  using tuple_type = std::decay_t<decltype(t)>;
+  using namespace meta::value_list;
+  return auto_named_tuple<element<typename tuple_type::names, indices>...,
+                          name>(value<indices>(std::forward<decltype(t)>(t))...,
+                                std::forward<decltype(x)>(x));
+}
+}  // namespace detail
+
+template <static_zstring name>
+constexpr auto push_back(forwardable::named_tuple auto&& t, auto&& value) {
+  return detail::push_back<name>(
+      std::forward<decltype(t)>(t), std::forward<decltype(value)>(value),
+      std::make_index_sequence<std::decay_t<decltype(t)>::names::size>{});
+}
+
+namespace detail {
+template <static_zstring name, std::size_t... indices>
+constexpr auto push_back_forward(forwardable::named_tuple auto&& t,
+                                 auto&& x,
+                                 std::index_sequence<indices...>) {
+  using tuple_type = std::decay_t<decltype(t)>;
+  using namespace meta::value_list;
+  return named_tuple_forward<element<typename tuple_type::names, indices>...,
+                             name>(
+      value<indices>(std::forward<decltype(t)>(t))...,
+      std::forward<decltype(x)>(x));
+}
+}  // namespace detail
+
+template <static_zstring name>
+constexpr auto push_back_forward(forwardable::named_tuple auto&& t, auto&& x) {
+  return detail::push_back_forward<name>(
+      std::forward<decltype(t)>(t), std::forward<decltype(x)>(x),
+      std::make_index_sequence<std::decay_t<decltype(t)>::names::size>{});
+}
+
+}  // namespace lyrahgames::xstd
---
+++ tests/main/named_tuple.cpp
@@ -0,0 +1,68 @@
+#include <string>
+//
+#include <doctest/doctest.h>
+//
+#include <lyrahgames/xstd/named_tuple.hpp>
+
+using namespace std;
+using namespace lyrahgames::xstd;
+
+static_assert(is_static_zstring_list<static_zstring_list<>>);
+static_assert(is_static_zstring_list<static_zstring_list<"first">>);
+static_assert(is_static_zstring_list<static_zstring_list<"first", "second">>);
+static_assert(!is_static_zstring_list<value_list<1, "second"_sz>>);
+
+SCENARIO("Named Tuple") {
+  auto package = auto_named_tuple<"id", "type", "value">(-1, 'c', 1.23f);
+  using package_type = decltype(package);
+
+  static_assert(package_type::contains<"id">);
+  static_assert(package_type::contains<"type">);
+  static_assert(package_type::contains<"value">);
+  static_assert(!package_type::contains<"xyz">);
+
+  CHECK(value<0>(package) == -1);
+  CHECK(value<1>(package) == 'c');
+  CHECK(value<2>(package) == 1.23f);
+
+  CHECK(value<"id">(package) == -1);
+  CHECK(value<"type">(package) == 'c');
+  CHECK(value<"value">(package) == 1.23f);
+
+  ++value<"id">(package);
+
+  CHECK(value<"id">(package) == 0);
+  CHECK(value<"type">(package) == 'c');
+  CHECK(value<"value">(package) == 1.23f);
+
+  value<"type">(package) = 'a';
+
+  CHECK(value<"id">(package) == 0);
+  CHECK(value<"type">(package) == 'a');
+  CHECK(value<"value">(package) == 1.23f);
+
+  package_type::names::for_each([&package]<auto name> {
+    MESSAGE(name.data() << " = " << value<name>(package));
+  });
+}
+
+SCENARIO("") {
+  auto package0 = auto_named_tuple<"message">("This is some information.");
+
+  int x = 1;
+  auto package1 = push_back<"severity">(package0, x);
+
+  auto package2 = push_back<"string">(package1, "Hello, World!"s);
+
+  // meta::print_type(package0);
+  // meta::print_type(package1);
+  // meta::print_type(package2);
+  // static_assert(meta::equal<decltype(package0)::types, type_list<const
+  // char*>>);
+  // MESSAGE(sizeof(package0));
+
+  // for_each with index set?
+  std::decay_t<decltype(package2)>::names::for_each([&]<auto name> {
+    MESSAGE(name.data() << " = " << value<name>(package2));
+  });
+}
